2eafaf6f85eced756d9959a7c9f166bfc214eb63
SPSpisoft <spring_software@yahoo.com>
2021-03-10 15:13:07 +0330

    debug

--- SpsCartButton/src/main/java/com/spisoft/spscartbutton/SpCartButton.java
+++ SpsCartButton/src/main/java/com/spisoft/spscartbutton/SpCartButton.java
@@ -634,7 +634,7 @@ public class SpCartButton extends RelativeLayout {
             }
     }
 
-    private void RefreshModeStatus(int mModeStatus, int mStatus) {
+    public void RefreshModeStatus(int mModeStatus, int mStatus) {
 //        if(CurrentMode != 2)
         if(mModel == 1 && mModeStatus == 0){
             mModeStatus = 1;
